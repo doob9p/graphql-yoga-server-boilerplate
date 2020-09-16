8ee8c0ceb2d7c1f2cdb08b7813f33eb9e8f579ad
cucumen <xnadi@alli-ver.com>
2020-09-16 21:56:24 +0900

    createPost 리졸버 생성

---
+++ src/graphql/Post/createPost/createPost.js
@@ -0,0 +1,19 @@
+import prisma from "../../../prisma";
+
+export default {
+  Mutation: {
+    createPost: async (_, args) => {
+      const { contents } = args;
+
+      try {
+        const post=await prisma.post.create({data: {contents}});
+
+        return post;
+      }catch(e) {
+        await prisma.$disconnect();
+
+        throw Error(e);
+      }
+    },
+  },
+};
